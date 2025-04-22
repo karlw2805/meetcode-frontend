d4ca0981577e90e05ce657760e1c0345177bd03d
krishsingla06 <148368716+krishsingla06@users.noreply.github.com>
2025-04-23 02:53:17 +0530

    Update Navbar.js : different buttons based on path

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./Navbar.css"; // Assuming you have a CSS file for styling
+import "./Navbar.css";
 import { Link, useLocation } from "react-router-dom";
 
 const Navbar = (props) => {
   const role = localStorage.getItem("role");
+  const location = useLocation();
+
   const handleLogout = () => {
     localStorage.removeItem("authToken");
     localStorage.removeItem("username");
     localStorage.removeItem("role");
     window.location.href = "/login";
   };
-  const location = useLocation();
-  const hideLogout =
+
+  const hideLogoutPath =
     location.pathname === "/login" || location.pathname === "/signin";
+
+  // Hide Home and Logout if role is workspace OR if path starts with /workspace/thesis
+  const hideNav =
+    role === "workspace" || location.pathname.startsWith("/workspace/thesis");
 
   return (
     <div>
@@ -24,23 +30,20 @@ const Navbar = (props) => {
         <div className="container-fluid">
           <div className="navbar-brand">{props.title}</div>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to={role === "student" ? "/" : "/admin"}
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                {/* <Link className="nav-link" to="/about">
-                  About
-                </Link> */}
-              </li>
-            </ul>
-            {!hideLogout && (
+            {!hideNav && (
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to={role === "student" ? "/" : "/admin"}
+                  >
+                    Home
+                  </Link>
+                </li>
+              </ul>
+            )}
+            {!hideLogoutPath && !hideNav && (
               <button className="login-btn" onClick={handleLogout}>
                 Logout
               </button>
