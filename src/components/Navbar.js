d068bd9d1d942fdd8614a3c2b0388d714b032dc0
krishsingla06 <148368716+krishsingla06@users.noreply.github.com>
2025-04-23 21:10:45 +0530

    Update Navbar.js - minor fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,9 +17,9 @@ const Navbar = (props) => {
   const hideLogoutPath =
     location.pathname === "/login" || location.pathname === "/signin";
 
-  // Hide Home and Logout if role is workspace OR if path starts with /workspace/thesis
+  // Hide Home and Logout if role is workspace OR if path starts with /workspace
   const hideNav =
-    role === "workspace" || location.pathname.startsWith("/workspace/thesis");
+    role === "workspace" || location.pathname.startsWith("/workspace");
 
   return (
     <div>
